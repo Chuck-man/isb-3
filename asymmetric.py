8c40ea8cbcb12a3127cd71e548c81352b617f844
Chuck-man <sgold2398@gmail.com>
2023-06-11 18:08:55 +0400

    Функция генерации ключей

--- asymmetric.py
+++ asymmetric.py
@@ -1,9 +1,23 @@
 import os
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes
 import logging
-import random
 
 logger = logging.getLogger()
 logger.setLevel('INFO')
+
+def gen_asymmetric_key() -> str:
+
+    keys = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+    private_key = keys
+    public_key = keys.public_key()
+    logging.info(' Сгенерированы ключи асимметричного шифрования')
+    return private_key, public_key
+
+
+    
+    
+    
 
