56e10c8aabef2cf0491b0456f0e76871e4529b9c
Chuck-man <sgold2398@gmail.com>
2023-06-12 23:44:05 +0400

    Удаление закомментированной части

--- asymmetric.py
+++ asymmetric.py
@@ -8,48 +8,6 @@ from cryptography.hazmat.primitives.serialization import (load_pem_private_key,
 logger = logging.getLogger()
 logger.setLevel("INFO")
 
-"""
-def asymmetric_keys_generation() -> tuple:
-    
-    Функция генерирует ключи для асимметричного шифрования
-    :return: закрытый ключ и открытый ключ
-    
-    keys = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048
-    )
-    private_key = keys
-    public_key = keys.public_key()
-    logging.info(f"Asymmetric keys successfully generated!")
-    return (private_key, public_key)
-
-
-def asymmetric_key_encryption(public_key, symmetric_key: bytes) -> bytes:
-    
-    Функция производит асимметричное шифрование по открытому ключу
-    :param symmetric_key: симметричный ключ для симметричного шифрования
-    :param public_key: открытый ключ для асимметричного шифрования
-    :return: зашифрованный симметричный ключ
-    
-    encrypted_symmetric_key = public_key.encrypt(symmetric_key, padding.OAEP(
-        mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
-    logging.info("Asymmetric encryption was successful!")
-    return encrypted_symmetric_key
-
-
-def asymmetric_key_decryption(private_key, symmetric_key: bytes) -> bytes:
-    
-    Функция расшифровывает асимметрично зашифрованный текст, с помощью закрытого ключа
-    :param symmetric_key: симметричный ключ для симметричного шифрования
-    :param private_key: закрытый ключ для асимметричного шифрования
-    :return: расшифрованный симметричный ключ
-    
-    decrypted_symmetric_key = private_key.decrypt(symmetric_key, padding.OAEP(mgf=padding.MGF1(
-        algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
-    logging.info("Asymmetric decryption was successful!")
-    return decrypted_symmetric_key
-"""
-
 class AsymmetricAlgorithm:
     def __init__(self, settings: dict) -> None:
         """
@@ -107,7 +65,7 @@ class AsymmetricAlgorithm:
         :public_pem (str): название файла, в который сохраняется публичный ключ
         :private_pem (str): название файла, в который сохраняется приватный ключ
         """
-        
+
         public_key_serialized = public_key.public_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PublicFormat.SubjectPublicKeyInfo
--- symmetric.py
+++ symmetric.py
@@ -7,53 +7,6 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 logger = logging.getLogger()
 logger.setLevel("INFO")
 
-
-"""
-def symmetric_key_generation() -> bytes:
-    
-    Функция генерирует ключ для симметричного шифрования
-    :return: ключ 
-    
-    key = os.urandom(16)
-    logging.info("Symmetric key successfully generated!")
-    return key
-
-
-def symmetric_encryption(key: bytes, text: bytes) -> bytes:
-    
-    Функция шифрует текст алгоритмом симметричного шифрования SEED
-    :param text: текст, который шифруем
-    :param key: ключ
-    :return: зашифрованный текст
-    
-    padder = padding.ANSIX923(128).padder()
-    padded_text = padder.update(text) + padder.finalize()
-    iv = os.urandom(16)
-    cipher = Cipher(algorithms.SEED(key), modes.CBC(iv))
-    encryptor = cipher.encryptor()
-    cipher_text = encryptor.update(padded_text) + encryptor.finalize()
-    logging.info("Symmetric encryption was successful!")
-    return iv + cipher_text
-
-
-def symmetric_decryption(key: bytes, cipher_text: bytes) -> bytes:
-    
-    Функция расшифровывает симметрично зашифрованный текст
-    :param cipher_text: зашифрованный текст
-    :param key: ключ
-    :return: возвращает расшифрованный текст
-    
-    iv = cipher_text[:16]
-    cipher_text = cipher_text[16:]
-    cipher = Cipher(algorithms.SEED(key), modes.CBC(iv))
-    decryptor = cipher.decryptor()
-    text = decryptor.update(cipher_text) + decryptor.finalize()
-    unpadder = padding.ANSIX923(128).unpadder()
-    result = unpadder.update(text) + unpadder.finalize()
-    logging.info("Symmetric decryption was successful!")
-    return result
-"""
-
 class SymmetricAlgorithm:
     def __init__(self, settings: dict) -> None:
         """
